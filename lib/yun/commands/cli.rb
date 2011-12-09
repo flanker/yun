4ff65a6031a34c3befe276ee116aeb346048bde3
FENG Zhichao <flankerfc@gmail.com>
2011-12-09 20:48:38 +1100

    use new model to implement `ssh` command

--- lib/yun/commands/cli.rb
+++ lib/yun/commands/cli.rb
@@ -10,7 +10,13 @@ module Yun
 
     desc "ssh NODE_NAME", "ssh to a node"
     def ssh node_name
-      yun.ssh node_name
+      key = "~/.ssh/#{Config.key_name}.pem"
+      user_name = "ec2-user"
+
+      node = connection.find node_name
+      server_ip = node.ip
+
+      exec "ssh -i #{key} #{user_name}@#{server_ip}"
     end
 
     register NodeCommand, :node, "node SUBCOMMAND", "commands for node"
