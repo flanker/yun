d2f577dad09e658f5b616c7289d465b998019f5b
FENG Zhichao <flankerfc@gmail.com>
2011-12-10 16:37:53 +1100

    fix the wrong user name in config test

--- spec/yun/model/ssh_config_spec.rb
+++ spec/yun/model/ssh_config_spec.rb
@@ -9,7 +9,7 @@ describe Yun::SshConfig do
   it 'should construct from config object' do
     ssh_config = Yun::SshConfig.new @key_name
 
-    ssh_config.user.should == 'ec2-user'
+    ssh_config.user.should == 'bitnami'
     ssh_config.key_file.should == '~/.ssh/some_key.pem'
   end
 
